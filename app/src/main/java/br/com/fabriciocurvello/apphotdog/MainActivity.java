5e575def0abf39eba84527fc1d1604f0ccc0d816
Vinicius Sousa <viniciusps.1190@gmail.com>
2024-09-01 22:57:37 -0300

    Terminando o projeto. Adicionando o retorno dos molhos, acompanhamentos, proteínas e correção de output.

--- app/src/main/java/br/com/fabriciocurvello/apphotdog/MainActivity.java
+++ app/src/main/java/br/com/fabriciocurvello/apphotdog/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -15,6 +16,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
     Switch swQueijoRalado;
     Button btPedido;
     TextView tvSaidaPedido;
-
 
 
     @Override
@@ -51,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
     } // fim do onCreate
 
 
-
     private void setupViews() {
         etNome = findViewById(R.id.et_nome);
         rgProteina = findViewById(R.id.rg_proteina);
@@ -69,10 +71,12 @@ public class MainActivity extends AppCompatActivity {
         btPedido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String cliente = nomeCliente();
-                String molhos = molhosSelecionados();
+                String cliente = getNomeCliente();
+                String molhos = getMolhosSelecionados();
+                String acompanhanmentos = getAcompanhamentosSelecionados();
+                String proteina = getProteina();
 
-                String pedido = cliente + molhos;
+                String pedido = cliente + "\n" + proteina + "\n" + molhos + "\n" + acompanhanmentos;
                 tvSaidaPedido.setText(pedido);
 
                 Toast.makeText(MainActivity.this, "Pedido enviado, " + cliente, Toast.LENGTH_SHORT).show();
@@ -81,19 +85,68 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private String nomeCliente() {
+    private String getNomeCliente() {
 
         String cliente = etNome.getText().toString(); // o toString() é para converter o texto em String, por incrível que pareça, o texto não vem como String.
         if (cliente.isEmpty()) {
-            cliente = "Não informou nome";
+            return "Cliente não identificado.";
         }
-        return "cliente: " + cliente;
+        return "Nome do cliente: " + cliente;
     }
 
-    private String molhosSelecionados() {
+    private String getProteina() {
+        int idProteina = rgProteina.getCheckedRadioButtonId();
+
+        if (idProteina == -1) { // inutil, pois um sempre vai estar selecionado.
+            return "Proteína não selecionada.";
+        }
+
+        RadioButton selectedButton = findViewById(idProteina);
+        String proteina = selectedButton.getText().toString();
 
 
-        return "Molhos selecionados: " + molhos;
+        return "Proteína: " + proteina;
     }
+
+    private String getMolhosSelecionados() {
+        List<String> molhos = new ArrayList<>(); // List é uma interface, não pode ser instanciada, por isso usamos o ArrayList
+
+        if (cbKetchup.isChecked()) {
+            molhos.add("Ketchup");
+        }
+        if (cbMostarda.isChecked()) {
+            molhos.add("Mostarda");
+        }
+        if (cbMaionese.isChecked()) {
+            molhos.add("Maionese");
+        }
+
+        if (molhos.isEmpty()) {
+            return "Não selecionou molhos.";
+        }
+
+
+        return "Molhos: " + String.join(", ", molhos) + ".";
+    }
+
+    private String getAcompanhamentosSelecionados() {
+        List<String> acompanha = new ArrayList<>();
+
+        if (swMilho.isChecked()) {
+            acompanha.add("Milho");
+        }
+        if (swErvilha.isChecked()){
+            acompanha.add("Evilha");
+        }
+        if (swQueijoRalado.isChecked()) {
+            acompanha.add("Queijo Ralado");
+        }
+
+        if (acompanha.isEmpty()){
+            return "Sem acompanhamentos.";
+        }
+        return "Acompanhamentos: " + String.join(", ", acompanha) + ".";
+    }
+
 
 }
